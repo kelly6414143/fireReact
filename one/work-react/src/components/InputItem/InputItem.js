3a31bde2a94acaa02dbeb0fca6009e1b9bb29165
kelly6414143 <kelly6414143@gmail.com>
2021-09-04 22:29:15 +0800

    註冊頁綁定form驗證

--- one/work-react/src/components/InputItem/InputItem.js
+++ one/work-react/src/components/InputItem/InputItem.js
@@ -3,25 +3,56 @@ import './inputItem.css'
 
 export default function InputItem(props) {
 
-    const { label, type, required, onChange, valid } = props
+    const { label, type, placeholder, required, hasIcon, onChange, onBlur, valid } = props
 
     const [isValid, setIsValid] = useState(true)
+    const [isEmpty, setIsEmpty] = useState(false)
+    const [isOpen, setIsOpen] = useState(false)
+    const [val, setVal] = useState("")
+
+    useEffect(() => {
+        if (valid?.isValid === undefined) return
+        setIsEmpty(!val)
+        setIsValid(valid?.isValid || valid && valid.testVal && valid.testVal(val))
+    }, [valid])
 
     return (
         <div className="input_item flex-row">
-            {required && <span style={{ color: '#FF0000' }}>+</span>}
+            {required && <span style={{ color: '#FF0000' }}>*</span>}
             <span className="input_label">{label}</span>
             <div className="input_value">
                 <input
-                    type={type}
-                    onChange={(event) => onChange(event.target.value)}
+                    type={type === 'password' ? isOpen ? 'text' : 'password' : type}
+                    placeholder={placeholder}
+                    onChange={(event) => {onChange(event.target.value); setVal(event.target.value)}}
                     onBlur={(event) => {
-                        console.log('blur')
-                        required && setIsValid(valid && valid.testVal.test(event.target.value))
+                        if (!required) return
+                        if (!event.target.value) {
+                            setIsEmpty(true)
+                            setIsValid(false)
+                        } else {
+                            let isCheck = true
+                            if (onBlur) {
+                                isCheck = onBlur(event.target.value)
+                            } else {
+                                isCheck = valid && valid.testVal ? valid.testVal(event.target.value) : true
+                            }
+
+                            setIsEmpty(false)
+                            setIsValid(isCheck)
+                        }
                     }}
-                    style={{ borderColor: !isValid && '#FF0000' }}
+                    style={{ border: !isValid && '2px solid #FF0000', width: '300px', padding: '10px 5px' }}
                 />
-                {required && valid && !isValid && <span style={{color: '#FF0000'}}>{valid.msg || '驗證錯誤囉'}</span>}
+                {hasIcon && (
+                    <span className={'icon_word'} onClick={() => setIsOpen(!isOpen)}> {isOpen ? '隱藏' : '顯示'}</span>
+                )}
+                {required && (
+                    <div className={'valid_msg'}>
+                        {isEmpty ? <span >{'請輸入內容'}</span> :
+                            valid && !isValid && <span>{valid.msg || '格式有誤'}</span>}
+                    </div>
+                )}
             </div>
         </div>
     );
--- one/work-react/src/pages/Register.js
+++ one/work-react/src/pages/Register.js
@@ -1,54 +1,129 @@
 import React, { useState, useEffect } from "react"
 import InputItem from "../components/InputItem/InputItem"
 
-export default function Register() {
+export default function Register(props) {
 
-    const [username, setUsername] = useState("")
-    const [password, setPassword] = useState("")
-    const [comfirmPassword, setComfirmPassword] = useState("")
+    const {history} = props
+    const [formObject, setFormObject] = useState({})
 
-    const onSumit = () => {
+    useEffect(() => {
+        setFormObject({
+            data: {
+                username: "",
+                password: "",
+                comfirmPassword: ""
+            },
+            vaildator: {
+                username: (el) => /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/.test(el),
+                password: (el) => /^[A-z]\d{2,6}[A-z]$/.test(el),
+            },
+            error: {}
+        })
+    }, [])
+
+    const onCheckComfirmPassword = (el) => {
+        return formObject?.data?.password === el && el !== ""
+    }
+
+    const onSubmit = async (event) => {
+        event.preventDefault()
+        let isValid = await onValidtor()
+        if (!isValid) return
         fetch('/api/register', {
             headers: {
                 "Content-Type": "application/json"
             },
             method: 'post',
             body: JSON.stringify({
-                username: username,
-                password: comfirmPassword
+                username: formObject.data.username,
+                password: formObject.data.password
             }),
         }).then(res => {
-            console.log('res', res)
+            if(res.status === 200){
+                onLogin(formObject.data)
+            }
         }).catch(err => {
-            console.log('err', err)
+            console.error('err', err)
+            sessionStorage.removeItem('userToken')
         })
     }
 
+    const onLogin = (data) => {
+        fetch('/api/login', {
+            headers: {
+                "Content-Type": "application/json"
+            },
+            method: 'post',
+            body: JSON.stringify({
+                username: data.username,
+                password: data.password
+            }),
+        }).then(res => {
+            return res.json()
+        }).then(res=>{
+            console.log('res',res)
+            if(res.success){
+                sessionStorage.setItem('userToken', res.token)
+                history.push('./')
+            }
+        }).catch(err => {
+            console.error('err', err)
+            sessionStorage.removeItem('userToken')
+        })
+    }
+
+    const onValidtor = async () => {
+        let errorObject = {}
+        let isValid = true
+
+        for (let i = 0, len = Object.keys(formObject.data).length; i < len; i++) {
+            const el = Object.keys(formObject.data)[i]
+
+            if (!formObject.data[el]) isValid = false
+            if (el === "comfirmPassword") {
+                isValid = onCheckComfirmPassword(formObject.data[el])
+                errorObject[el] = onCheckComfirmPassword(formObject.data[el])
+            }
+
+            if (formObject.vaildator[el] !== undefined && formObject.vaildator[el]) {
+                errorObject[el] = formObject.vaildator[el](formObject.data[el])
+            }
+        }
+        setFormObject({ ...formObject, error: { ...formObject.error, ...errorObject } })
+        return Object.keys(errorObject).filter(el => !errorObject[el]).length === 0 && isValid
+    }
 
     return (
         <div>
-            <header>Register</header>
+            <header>註冊</header>
             <InputItem
                 label="帳號"
                 type="text"
                 required={true}
-                onChange={(val) => setUsername(val)}
-                valid={{msg:'錯囉', testVal: /ab+c/}}
+                placeholder={'必須是信箱'}
+                onChange={(val) => setFormObject({ ...formObject, data: { ...formObject.data, username: val } })}
+                valid={{ msg: '必須是信箱', testVal: formObject?.vaildator?.['username'], isValid: formObject?.error?.['username'] }}
             />
             <InputItem
                 label="密碼"
                 type="password"
                 required={true}
-                onChange={(val) => setPassword(val)}
-                valid={{msg:'', testVal: /ab+c/}}
+                hasIcon={true}
+                placeholder={'4-8字元；首尾必須是英文；中間必須是數字'}
+                onChange={(val) => setFormObject({ ...formObject, data: { ...formObject.data, password: val } })}
+                valid={{ msg: '4-8字元；首尾必須是英文；中間必須是數字', testVal: formObject?.vaildator?.['password'], isValid: formObject?.error?.['password'] }}
             />
             <InputItem
                 label="確認密碼"
                 type="password"
                 required={true}
-                onChange={(val) => setComfirmPassword(val)}
+                hasIcon={true}
+                placeholder={'4-8字元；首尾必須是英文；中間必須是數字'}
+                onChange={(val) => setFormObject({ ...formObject, data: { ...formObject.data, comfirmPassword: val } })}
+                valid={{ msg: '與密碼不同', testVal: formObject?.vaildator?.['password'], isValid: formObject?.error?.['comfirmPassword'] }}
+                onBlur={(val) => onCheckComfirmPassword(val)}
             />
-            <button onClick={onSumit}>確認</button>
+            <button onClick={onSubmit}>註冊</button>
         </div>
     );
 }
