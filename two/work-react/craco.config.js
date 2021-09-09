4e95f625c245ba1c70b95c19010f25f6f8f4b9f6
kelly6414143 <kelly6414143@gmail.com>
2021-09-09 22:11:40 +0800

    設置alias路徑, 並套用在login頁

--- two/work-react/craco.config.js
+++ two/work-react/craco.config.js
@@ -1,10 +1,18 @@
+const path = require('path');
+
+console.log('craco')
+
 module.exports = {
-    style: {
-      postcss: {
-        plugins: [
-          require('tailwindcss'),
-          require('autoprefixer'),
-        ],
-      },
+  webpack: {
+    alias: {
+      "@": path.join(path.resolve(__dirname, "./src")),
+      "@components": path.join(path.resolve(__dirname, "./src/components")),
+      "@api": path.join(path.resolve(__dirname, "./src/api")),
     },
-  }
+  },
+  style: {
+    postcss: {
+      plugins: [require("tailwindcss"), require("autoprefixer")],
+    },
+  },
+};
--- two/work-react/src/pages/Login.js
+++ two/work-react/src/pages/Login.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from "react"
-import InputItem from "../components/InputItem/InputItem"
-import toast from "../components/Toast/Toast"
-import Validator from "../components/Validator"
-import api from '../api/index'
+import InputItem from "@components/InputItem/InputItem"
+import toast from "@components/Toast/Toast"
+import Validator from "@components/Validator"
+import api from '@api/index'
 
 export default function Login(props) {
 
