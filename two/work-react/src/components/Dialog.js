b69c1505604d49561c79d57053578d3d6f9ad9ef
kelly6414143 <kelly6414143@gmail.com>
2021-09-11 22:42:54 +0800

    移除console.log

--- two/work-react/src/components/Dialog.js
+++ two/work-react/src/components/Dialog.js
@@ -3,8 +3,6 @@ import React from "react";
 export default function Dialog(props) {
     const { isShowDialog, children } = props
 
-    console.log(isShowDialog)
-
     return (
         isShowDialog && (<div className="absolute inset-0 bg-black bg-opacity-20 flex flex-col justify-center items-center">
             {children}
