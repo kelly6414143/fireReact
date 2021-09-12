4f009eebeaae39f16c9a2b0f076962187acd8e33
kelly6414143 <kelly6414143@gmail.com>
2021-09-12 22:06:12 +0800

    header增加使用者資訊

--- two/work-react/src/components/Layout/Header/index.js
+++ two/work-react/src/components/Layout/Header/index.js
@@ -1,17 +1,23 @@
 import React from "react";
 import { withRouter } from "react-router-dom";
+import { useContextSelector } from 'use-context-selector';
+import { context } from '@/stores/context'
 
 function Header(props) {
 
-  const { history: {replace}, containerClassName } = props
+  const { history: { replace }, containerClassName } = props
+
+  const userInfo = useContextSelector(context, state => state.userInfo[0]);
 
   return (
     <div className={`flex justify-between items-center p-6 border border-gray-800 ${containerClassName}`}>
-      <div className="text-base font-bold cursor-pointer" onClick={()=> replace("/")}>LOGO</div>
-      <div onClick={() => {
-        sessionStorage.removeItem('userToken')
-        replace('/login')
-      }}>登出</div>
+      <div className="text-base font-bold cursor-pointer" onClick={() => replace("/")}>LOGO</div>
+      <div className="flex items-center">
+        <span className="mx-2 text-xs">{`${userInfo.name}(${userInfo.username})`}</span>
+        <div onClick={() => {
+          replace('/login')
+        }}>登出</div>
+      </div>
     </div>
   );
 }
