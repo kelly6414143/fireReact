828fd9de1a964ca8507487a14357209d33f8a3a6
kelly.cyc <kelly.cyc@link8tech.tw>
2021-09-15 18:13:36 +0800

    調整跳窗顯示

--- two/work-react/src/components/Dialog.js
+++ two/work-react/src/components/Dialog.js
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 export default function Dialog(props) {
-    const { isShowDialog, children } = props
+    const { isShowDialog = true, children } = props
+
+    const [isShow, setIsShow] = useState(true)
+
+    useEffect(()=>{
+        
+    },[])
+
+    useEffect(()=>{
+        setIsShow(isShowDialog)
+    },[isShowDialog])
 
     return (
-        isShowDialog && (<div className="absolute inset-0 bg-black bg-opacity-20 flex flex-col justify-center items-center">
-            {children}
-        </div>)
+        <div>
+            {isShow && <div
+                className="absolute inset-0 bg-black bg-opacity-20 flex flex-col justify-center items-center"
+                onClick={() => setIsShow(false)}
+            >
+                {children}
+            </div>}
+        </div>
     );
 }
