ed746249a6c64f534c42d704b6f4eee0ceb5efb4
kelly6414143 <kelly6414143@gmail.com>
2021-09-21 22:02:39 +0800

    表單用form/input組件取代

--- two/work-react/src/components/Layout/Layout.js
+++ two/work-react/src/components/Layout/Layout.js
@@ -1,25 +1,37 @@
 import React, { useEffect, useState } from "react";
 import { useContextSelector } from "use-context-selector";
 import { context } from "@/stores/context";
+import FieldContext from "@/stores/FieldContext"
 import api from "@api/index";
 import toast from "@components/Toast/Toast";
 import Modal from "@/components/Modal";
-import InputItem from "@components/InputItem/InputItem";
+import Input from "@components/InputItem/Input";
+import Form, { Field } from "@components/Form";
 import Header from "./Header/index";
 import Drawer from "./Drawer";
 
-export default function Wrapper(props) {
-  const {
-    history: { replace },
-    routes,
-    children,
-  } = props;
+export default function Wrapper({
+  history: { replace },
+  routes,
+  children,
+}) {
+
+  const nameRules = { required: true, msg: "請輸入使用者名稱" }
+
+  const [form] = Form.useForm()
 
   const [isShowDialog, setIsShowDialog] = useState(false);
-  const [username, setUsername] = useState(undefined);
 
   const userInfo = useContextSelector(context, (state) => state.userInfo[0]);
   const setUserInfo = useContextSelector(context, (state) => state.userInfo[1]);
+
+  const onFinish = (data) => {
+    onSubmit(data)
+  }
+
+  const onFinishFailed = (val) => {
+    // React.$commonTool.devConsole("onFinishFailed", val)
+  }
 
   useEffect(() => {
     if (userInfo.name) return;
@@ -44,11 +56,11 @@ export default function Wrapper(props) {
       });
   }, []);
 
-  const onSubmit = () => {
+  const onSubmit = (data) => {
     api()
       .put(
         "/api/users/updateName",
-        { name: username },
+        data,
         {
           headers: {
             ContentType: "application/x-www-form-urlencoded",
@@ -59,6 +71,7 @@ export default function Wrapper(props) {
       .then((res) => {
         if (res.success) {
           toast.success(res.message);
+          setUserInfo({...userInfo, name: data.name})
           setIsShowDialog(false);
         } else {
           toast.error(res.message);
@@ -80,27 +93,25 @@ export default function Wrapper(props) {
         isShowModal={isShowDialog}
         isShowHeader={true}
         title={"使用者資料補全"}
-        onBackDropClick={()=> {return false}}
-        ModalActions={<button
-          className=" bg-blue-500 rounded-lg px-3 py-1 m-1 text-white"
-          onClick={onSubmit}
-        >
-          確定
-        </button>}
+        onBackDropClick={() => { return false }}
+        ModalActions={<></>}
       >
-        <div className="relative bg-white px-8 py-6 text-center">
-          <section className="mb-4 mt-6">
-            <InputItem
+        {/* <div className="relative bg-white px-8 py-6 text-center"> */}
+        <Form className="text-center" form={form} onFinish={onFinish} onFinishFailed={onFinishFailed}>
+          <Field name="name" rules={[nameRules]}>
+            <Input
               label="使用者名稱"
               type="text"
-              required={true}
               placeholder={"對其他用戶顯示的名稱"}
-              onChange={(val) => setUsername(val)}
-              valid={{ isValid: username }}
-              labelClassName="w-auto"
             />
-          </section>
-        </div>
+          </Field>
+          <input
+            type="submit"
+            className=" bg-blue-500 rounded-lg px-3 py-1 m-1 text-white"
+            value={'確定'}
+          />
+        </Form>
+        {/* </div> */}
       </Modal>
     </div>
   );
