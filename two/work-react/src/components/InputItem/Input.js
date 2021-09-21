390fedf4e45ee294b019cc271f1d8beb8c5bae3a
kelly6414143 <kelly6414143@gmail.com>
2021-09-21 23:32:49 +0800

    增加type

--- two/work-react/src/components/InputItem/Input.js
+++ two/work-react/src/components/InputItem/Input.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react'
 
 export default function Input(props) {
 
-    const { value, label, required, placeholder, onChange, labelClassName, err } = props
+    const { value, type, label, required, placeholder, onChange, labelClassName, err } = props
 
     return (
         <div className="flex flex-row items-center justify-between my-6">
@@ -14,6 +14,7 @@ export default function Input(props) {
                 <input
                     value={value || ""}
                     placeholder={placeholder}
+                    type={type || 'text'}
                     onChange={onChange}
                     style={{ border: err ? '1px solid #FF0000' : '1px solid #DDDDDD', width: '300px', fontSize: '13px', padding: '10px 5px' }}
                 />
