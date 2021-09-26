fd41be6800a55dea337f861425d8576e96e967c2
kelly6414143 <kelly6414143@gmail.com>
2021-09-26 20:56:14 +0800

    排除tailwind intellisence異常

--- one/work-react/tailwind.config.js
+++ one/work-react/tailwind.config.js
@@ -1,3 +1,5 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
@@ -11,9 +13,10 @@ module.exports = {
         'full': '100%',
         '50': '50px',
         '100': '100px',
-        '450': '450px'
-       },
-       height: {
+        '450': '450px',
+        'minContent': 'min-content'
+      },
+      height: {
         '0': '0',
         '1/4': '25%',
         '1/2': '50%',
@@ -22,7 +25,7 @@ module.exports = {
         '50': '50px',
         '100': '100px',
         '450': '450px'
-       },
+      },
       minWidth: {
         '0': '0',
         '1/4': '25%',
@@ -30,25 +33,40 @@ module.exports = {
         '3/4': '75%',
         'full': '100%',
         '50': '50px',
-        '100': '100px'
-       },
-       maxWidth: {
+        '100': '100px',
+        '450': '450px',
+        'minContent': 'min-content'
+      },
+      maxWidth: {
         '0': '0',
         '1/4': '25%',
         '1/2': '50%',
         '3/4': '75%',
         'full': '100%',
-       },
-       fontSize: {
+        '50': '50px',
+        '100': '100px',
+        '450': '450px'
+      },
+      maxHeight: {
+        '0': '0',
+        '1/4': '25%',
+        '1/2': '50%',
+        '3/4': '75%',
+        'full': '100%',
+        '50': '50px',
+        '100': '100px',
+        '450': '450px'
+      },
+      fontSize: {
         sm: ['12px', '20px'],
         base: ['16px', '24px'],
         lg: ['20px', '28px'],
         xl: ['24px', '32px'],
       },
-      inset:{
+      inset: {
         '0': '0',
         '1': '4px',
-        '2' : '8px',
+        '2': '8px',
         '1/2': '50%',
         'full': '100%'
       },
